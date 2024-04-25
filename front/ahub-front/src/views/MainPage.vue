c1a689a663205c0c1c3b4ba490206ad505e468e7
t4e1 <jtw5738@naver.com>
2024-04-25 15:06:07 +0900

    Main 에 카카오 주소 api 적용

--- front/ahub-front/src/views/MainPage.vue
+++ front/ahub-front/src/views/MainPage.vue
@@ -1,14 +1,28 @@
 <template>
-    <Banner/>
+    <Banner />
     <div style="width: 100%; height: 800px;">
+
+        <input type="text" id="sample4_postcode" placeholder="우편번호">
+        <input type="button" @click="findAddress()" value="우편번호 찾기"><br>
+
 
     </div>
 </template>
 
 <script setup>
-    import Banner from '@/components/Banner/Banner.vue'
+import Banner from '@/components/Banner/Banner.vue'
+
+function findAddress() {
+    new window.daum.Postcode({
+        oncomplete: (data) => {
+            console.log("받은 주소 : ", data)
+            this.zoneCode = data.zonecode;
+            this.address01 = data.address;
+        }
+
+    }).open()
+}
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
